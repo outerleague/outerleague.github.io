71d34d834fb4cefe273bceb0a4430b9ba6e95efa
Shura125 <itsshura125@gmail.com>
2025-08-25 00:41:39 -0500

    First Push for Outer League

---
+++ assets/CarianManor-CM8BzOTf.js
@@ -0,0 +1,73 @@
+import{_ as p,N as b}from"./OuterLeagueLogo-Bq_WKjt0.js";import{d as C,T as w}from"./marked.esm-Cupjb65a.js";import{_ as m,c as h,o as g,a as s,b as _,d as y,e as f,f as u,g as c}from"./index-B2-VNebP.js";const v={name:"CarianManorRules",setup(){const i=`
+# Outer League Carian Manor
+Ranni has moved to the Cathedral of Manus Celes, which has left Carian Manor abandoned. *Oedon* believes that Ranni could be a powerful ally. So as a gesture of good faith you are tasked to Hitless Carian Manor and defeat the Full Grown Fallingstar Beast to take it back for Ranni. 
+
+Oedon will be handing out **Cosmic Embers** for those who can overcome this challenge.
+
+The Goal is simple: Complete **Carian Manor** Hitless. Due to *Oedon's* Arrival, some items and weapons work differently, so make sure to test everything out to accomplish a run. 
+
+You can get a **Cosmic Ember** by:
+- Completing a Run Hitless
+- Completing the *Elite Enemies Challenge* Hitless
+- Completing a Run under 5 Minutes
+
+Only **Cosmic Ember** with a deadline for <t:1757307540:F>:
+- Having the Most Creative Hitless Kill on the Full Grown Fallingstar Beast voted on by the community
+
+This does mean that an Individual Player can get a total of 4 **Cosmic Embers** 
+
+### Elite Enemies Challenge
+There are 16 Elite Enemies and 8 Unique Killing Sorceries
+
+The 8 Unique Sorceries are:
+- Cannon of Hiama
+- Fleeting Microcosm
+- Glintblade Trio
+- Shard Spiral
+- Comet
+- Great Oracle Bubble
+- Adula's Moonblade
+- Rennala's Full Moon
+
+The 16 Elite Enemies: https://youtu.be/iaMoRg3gdYA?si=AjXLt2KGHwoIfVpV
+
+First Floor
+- Zamor
+- Scaly Misbegotten
+- T-Rex Dog
+- Omenkiller
+- Bloodhound Knight
+- Albunaric Archer
+- Carian Soldier
+- Lion Guardian
+- Kyle's Cousin
+
+Bridges Floor
+- Banished Knight Halberd
+- Ancestral Warrior
+
+Second Floor
+- Zamor
+- Banished Knight Shield
+- Bloodhound Knight
+- Crab
+- Skeletal Beastman
+
+
+Each Sorcery must Kill 2 Unique Elite Enemies and all Damage done must be from that sorcery for those Elite Enemies. Meaning no Fall Damage. And As always, you must defeat the Full Grown Fallingstar Beast at the end.
+
+### Creativity Challenge
+The Community will vote on the Most Creative Full Grown Fallingstar Beast Kill.
+For this Challenge, only the Boss Kill needs to be Hitless, So make sure to grab what you want to use and make a Save State to get the best clip. Submissions will close on <t:1757307540:F>
+
+**Rules:**
+- No Leveling
+- No Roundtable
+- No Gathering Items or Resources Outside Carian Manor
+- You can Only use the Sorceries Provided inside and no other Weapon
+- No Glitches or Cheats
+- Players are allowed to attempt as many times as they want
+- I will be Referring to The Team Hitless Rules, So obvious stuff like No Quitouts
+- Completing Carian Manor means to get to and Defeat the Loretta Replacement Hitless using a Fresh New Character, You are allowed to Quit Out when you see "RUN COMPLETED" (This Text Replaces the "Enemy Felled" Text)
+
+    `,e=_("");function l(n){return n.replace(/<t:(\d+)(?::([a-zA-Z]))?>/g,(t,o,d)=>{const a=new Date(parseInt(o,10)*1e3);switch(d){case"t":return a.toLocaleTimeString();case"T":return a.toLocaleTimeString([],{hour:"2-digit",minute:"2-digit",second:"2-digit"});case"d":return a.toLocaleDateString();case"D":return a.toLocaleDateString([],{year:"numeric",month:"long",day:"numeric"});case"f":return a.toLocaleString([],{dateStyle:"long",timeStyle:"short"});case"F":return a.toLocaleString([],{dateStyle:"full",timeStyle:"long"});case"R":return r(a);default:return a.toLocaleString()}})}function r(n){const t=(n.getTime()-Date.now())/1e3,o=new Intl.RelativeTimeFormat("en",{numeric:"auto"});return Math.abs(t)<60?o.format(Math.round(t),"seconds"):Math.abs(t)<3600?o.format(Math.round(t/60),"minutes"):Math.abs(t)<86400?o.format(Math.round(t/3600),"hours"):o.format(Math.round(t/86400),"days")}return y(()=>{const n=l(i);e.value=C.parse(n)}),{parsedText:e}}},M={class:"rules-container"},S=["innerHTML"];function T(i,e,l,r,n,t){return g(),h("div",M,[s("div",{innerHTML:r.parsedText},null,8,S)])}const F=m(v,[["render",T],["__scopeId","data-v-67afa7c8"]]),E=f({name:"GithubDownloadCard",methods:{openGithub(){window.open("https://github.com/ignitesouls/Outer-League/releases/tag/Carian-Manor","_blank")}}}),R={class:"card"};function D(i,e,l,r,n,t){return g(),h("div",R,[e[1]||(e[1]=s("h2",{class:"title"},"Carian Manor",-1)),e[2]||(e[2]=s("p",{class:"description"},"Click below to download the latest version from GitHub.",-1)),s("button",{onClick:e[0]||(e[0]=(...o)=>i.openGithub&&i.openGithub(...o)),class:"download-btn"}," Download from GitHub ")])}const $=m(E,[["render",D],["__scopeId","data-v-47f7884a"]]),G=f({name:"CarianManor",components:{Navigation:b,Table:w,Info:F,Download:$}}),H={class:"page-container"},L={class:"main-container"};function k(i,e,l,r,n,t){const o=c("Navigation"),d=c("Info"),a=c("Download");return g(),h("div",H,[u(o),s("div",L,[e[0]||(e[0]=s("div",{class:"top-image-container"},[s("img",{src:p,class:"logo"})],-1)),u(d),u(a)])])}const O=m(G,[["render",k],["__scopeId","data-v-805f42cf"]]);export{O as default};
